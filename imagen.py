660676538122b331452393b70172c13007de0baa
Riccardo02 <31690722+Riccardo02@users.noreply.github.com>
2019-12-27 10:33:36 +0100

    Added files via upload

---
+++ imagen.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+
+from astropy.io import fits
+import matplotlib.pyplot as plt
+indirizzo='/home/riccardo/Desktop/Esopianeti/level_1.fits'
+hdu=fits.open(indirizzo)
+data=hdu[0].data
+ima=data[6,:,:]
+plt.imshow(-ima,cmap='ocean')
+
+for i in range(1000):
+	print (i)
+	ima=data[i,:,:]
+	plt.imshow(-ima,cmap='ocean')
+	plt.plot([1,2,3])
+	plt.savefig(str(i)+'.png')
+
+
+print (len(data))
+
+
+plt.show()
